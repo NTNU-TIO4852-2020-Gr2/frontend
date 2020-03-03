b0a0b3f5d78956f850190ad91cd80804a02c2353
Håvard O. Nordstrand <haavard@haavard.tech>
2020-03-03 04:42:13 +0100

    Update indentation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,33 @@
 let app = new Vue({
-    el: "#app",
-    data: {
-        siteTitle: "PH2O",
-        siteRootUrl: "./",
-        apiRootUrl: "./",
-        overviewAllOkay: true,
-        activeDeviceUuid: "",
-        devices: [
-            {
-                uuid: "1",
-                name: "First",
-                timeCreated: 0,
-                latitude: 0,
-                longitude: 0,
-            },
-            {
-                uuid: "2",
-                name: "Second",
-                timeCreated: 0,
-                latitude: 0,
-                longitude: 0,
-            },
-            {
-                uuid: "3",
-                name: "Third",
-                timeCreated: 0,
-                latitude: 0,
-                longitude: 0,
-            },
-        ],
-    }
+  el: "#app",
+  data: {
+    siteTitle: "PH2O",
+    siteRootUrl: "./",
+    apiRootUrl: "./",
+    overviewAllOkay: true,
+    activeDeviceUuid: "",
+    devices: [
+      {
+        uuid: "1",
+        name: "First",
+        timeCreated: 0,
+        latitude: 0,
+        longitude: 0,
+      },
+      {
+        uuid: "2",
+        name: "Second",
+        timeCreated: 0,
+        latitude: 0,
+        longitude: 0,
+      },
+      {
+        uuid: "3",
+        name: "Third",
+        timeCreated: 0,
+        latitude: 0,
+        longitude: 0,
+      },
+    ],
+  }
 });
